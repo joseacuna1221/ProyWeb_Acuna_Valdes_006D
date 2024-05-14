953c402f94f42cd51afc8df2ed8f83f3da424958
joseacuna1221 <134325342+joseacuna1221@users.noreply.github.com>
2024-05-14 07:51:32 -0400

    Entrega 2 – Programando Front-End

--- js/funcionesM.js
+++ js/funcionesM.js
@@ -1,36 +1,26 @@
-function getPerros(numPerros, done) {
-    fetch(`https://dog.ceo/api/breed/hound/images/random/${numPerros}`)
+function mostrarMascotas() {
+    let url = 'http://localhost:3300/nombres';
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            done(data);
-        })
-        .catch(error => {
-            console.error('Error al obtener las imágenes de perros:', error);
-        });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const numPerros = 9;
-    getPerros(numPerros, data => {
-        if (data.status === "success") {
-            const perros = data.message;
-            
-            perros.forEach(perroUrl => {
+            data.forEach(perro => {
                 const article = document.createRange().createContextualFragment(/*html*/`
                     <article>
                         <div class="image-container" style="max-height: 400px;">
-                            <img src="${perroUrl}" alt="perro">
+                            <img src="${perro.imagen}" alt="${perro.nombre}">
                         </div>
-                        <h2>Perro</h2>
-                        <span>Perro</span>
+                        <h2>${perro.nombre}</h2>
+                        <span>${perro.estado}</span>
                     </article>
                 `);
 
                 const main = document.querySelector("main");
                 main.append(article);
             });
-        } else {
-            console.error('Error al obtener las imágenes de perros');
-        }
-    });
+        })
+        .catch(error => console.log(error))
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarMascotas();
 });
